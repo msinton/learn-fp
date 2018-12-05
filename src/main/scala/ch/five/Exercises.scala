ac5a2dd5678a3c79f23f34f7bda72f5d4033702a
Matt Sinton-Hewitt <matt.sinton-hewitt@ovoenergy.com>
2018-12-05 09:17:17 +0000

    Complete exercises for ch5

--- src/main/scala/ch/five/Exercises.scala
+++ src/main/scala/ch/five/Exercises.scala
@@ -2,15 +2,34 @@ package ch.five
 
 object Exercises {
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, rng2) = rng.nextInt
+    if (i < 0) (math.abs(i + 1), rng2)
+    else (i, rng2)
+  }
  
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (i1, rng2) = nonNegativeInt(rng)
+    (i1.toDouble / (Int.MaxValue + 1), rng2)
+  }
 
-  def intDouble(rng: RNG): ((Int, Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+//    val (i, rng2) = rng.nextInt
+    val a = rng.nextInt
+    val b = double(a._2)
+    ((a._1, b._1), b._2)
+  }
  
-  def doubleIn(rng: RNG): ((Double, Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = ???
 
   def double3(rng: RNG): ((Double, Double, Double), RNG) = ???
- 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    (0 until count).foldLeft((List.empty[Int], rng)) {
+      case ((xs, nextRng), _) =>
+        val (i, rng2) = nextRng.nextInt
+        (xs :+ i, rng2)
+    }
+  }
+
 }
